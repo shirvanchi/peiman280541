dd040254179b33c1a38e61eaf81ddcb4693f3bf9
rashidi123 <shirvanchi.p@gmail.com>
2019-01-05 18:33:23 +0330

    change program

---
+++ prog1.sh
@@ -0,0 +1,12 @@
+echo "1- create file"
+echo "2- list of fileread input"
+echo "3- exit"
+echo "please select item"
+if (($choice==1))
+then 
+echo "file name" 
+sudo touch $name
+if (($choice==2))
+then
+ls -lah
+fi
